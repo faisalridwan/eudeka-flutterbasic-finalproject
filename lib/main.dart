552cb6731ca82ea8f80a00949aee6b3c608f6236
Faisal Ridwan <faisalridwansiregar@gmail.com>
2019-12-20 14:27:18 +0700

    add icon

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Drink App',
+      title: 'Drinks',
       theme: ThemeData(
 
         primarySwatch: Colors.red,
